09e389a6e34f37b95cc73442b509f03bb2ea44b2
guildyyy <guildysource@gmail.com>
2023-05-27 12:58:57 -0500

    Restart working on the Rock paper Scissor game

--- rockPaperScissors/rockPaperScissor.js
+++ rockPaperScissors/rockPaperScissor.js
@@ -33,58 +33,163 @@ function getComputerChoice()
 
 function playRound(playerSelection,computerSelection)
 {
-    const playerChoice = playerSelection.toUpperCase();
+    const playerChoice = playerSelection
     const computerChoice = computerSelection;
-    let a = "It's a tie! Play again!"
+
+    //tie =0; lose = 1; win = 2;
+
+    let a = 0;
+
     if (playerChoice === "ROCK")
     {
         if (computerChoice === "PAPER")
         {
-            a = "You have lost. Rock loses to paper everytime!";
+            a = 1;;
+        
         }
         else if (computerChoice === "SCISSORS")
         {
-            a = "You win! Scissors cannot cut rocks!";
+            a = 2;
+   
         }
     }
     else if (playerChoice === "PAPER")
     {
         if (computerChoice === "ROCK")
         {
-            a = "You win! Your paper has embraced the rock to submission!";
+            a = 2;
+  
         }
         else if (computerChoice === "SCISSORS")
         {
-            a = "You have lost. Paper does not stand a chance against the sharp edges of a scissor.";
+            a = 1;
+
         }
     }
     else if (playerChoice === "SCISSORS")
     {
         if (computerChoice === "ROCK")
         {
-            a = "You have lost. Scissors get dull when the try to cut rocks."
+            a = 1;
         }
         else if (computerChoice === "PAPER")
         {
-            a = "You have won! Like a hot knife through butter, your scissor cuts through paper!"
+            a = 2;
         }
     }
 
     return a;
 }
 
-function game()
+
+
+// const button = document.getElementById('button');
+// button.addEventListener('click', game);
+
+let winCounter = 0;
+let loseCounter = 0;
+let paraText = "";
+let paraWinnerText = "";
+
+
+
+const button1 = document.createElement('div');
+button1.classList.add('buttons');
+button1.innerText = "Rock";
+button1.value = "ROCK";
+button1.addEventListener('click', function()
 {
+    const computerSelection = getComputerChoice();
+    const result = playRound("ROCK",computerSelection);
 
-    for (let i = 0; i< 5; i++)
+    if (result === 0)
     {
-        const playerSelection = prompt("Choose between rock, paper, and scissors.");
-        const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection,computerSelection));
+        paraText = "Tie";
     }
+    else if( result === 1)
+    {
+        loseCounter++;
+        paraText = "Lose";
+    }
+    else if (result === 2)
+    {
+        winCounter++;
+        paraText = "Win"
+    }
+    para.innerText = paraText;
+    checkWinner(winCounter,loseCounter);
+
+});
+
+const button2 = document.createElement('div');
+button2.classList.add('buttons');
+button2.innerText = "Paper";
+button2.value = "PAPER";
+button2.addEventListener('click', function()
+{
+    const computerSelection = getComputerChoice();
+    const result = playRound("ROCK",computerSelection);
+
+    if (result === 0)
+    {
+        paraText = "Tie";
+    }
+    else if( result === 1)
+    {
+        loseCounter++;
+        paraText = "Lose";
+    }
+    else if (result === 2)
+    {
+        winCounter++;
+        paraText = "Win"
+    }
+    para.innerText = paraText;
+    checkWinner(winCounter,loseCounter);
+
+});
+
+const button3 = document.createElement('div');
+button3.classList.add('buttons');
+button3.innerText = "Scissors";
+button3.value = "SCISSORS";
+button3.addEventListener('click', function(e) 
+{
+    const computerSelection = getComputerChoice();
+    const result = playRound("ROCK",computerSelection);
+
+    if (result === 0)
+    {
+        paraText = "Tie";
+    }
+    else if( result === 1)
+    {
+        loseCounter++;
+        paraText = "Lose";
+    }
+    else if (result === 2)
+    {
+        winCounter++;
+        paraText = "Win"
+    }
+    para.innerText = paraText;
+    checkWinner(winCounter,loseCounter);
+
+});
+
+function checkWinner(playerScore, computerScore)
+{
+    if (playerScore === 5)
+    {
+        paraWinnerText = "Player Wins";
+        paraWinner.innerText = paraWinnerText;
+
+    }
+    else if (computerScore === 5)
+    {
+        paraWinnerText = "Computer Wins";
+        paraWinner.innerText = paraWinnerText;
+    }
+
 }
-
-const button = document.getElementById('button');
-button.addEventListener('click', game);
-
 
